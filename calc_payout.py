a73a4fd7e12eaf8b6a3505603e25e8b0638914f0
DoryGuy <gwpowell@hotmail.com>
2026-08-06 21:52:37 -0700

    add investing the savings each year.

--- calc_payout.py
+++ calc_payout.py
@@ -42,13 +42,13 @@ summer_effective_rate = summer_peak_rate - summer_super_off_peak_rate
 num_days_at_summer_rates = 153 # June 1 thru Oct 31
 
 new_battery_value_default = 20000
-new_battery_value = float(input(f"Enter new battery value (default {new_battery_value_default:.2f}) ").strip() or
+new_battery_value = float(input(f"Enter new battery system value (default {new_battery_value_default:.2f}) ").strip() or
                       new_battery_value_default)
 new_battery_annual_depreciation_default = 0.10
 new_battery_annual_depreciation = float(input(f"Enter new battery annual depreciation rate (default {new_battery_annual_depreciation_default:.2f}) ").strip() or
                       new_battery_annual_depreciation_default)
 battery_install_cost_default = 10500.00  # dollars
-battery_install_cost = float(input(f"Enter battery install cost (default {battery_install_cost_default:.2f}) ") or
+battery_install_cost = float(input(f"Enter your battery installation cost (default {battery_install_cost_default:.2f}) ") or
                          battery_install_cost_default)
 
 winter_savings = winter_effective_rate * battery_max_output * num_days_at_winter_rates
@@ -70,35 +70,44 @@ investment_annual_return_default = 0.05  # return on a safe investment
 investment_annual_return = float(input(f"Enter annual investment return (default {investment_annual_return_default}) ").strip() or
                             investment_annual_return_default)
 
+electrical_savings_investment = 0.0
+
 total_no_battery_cost = 0.0
 
 for year in range (1,21,1):
     print(f"Year {year}:")
-    print(f"    Possible savings: {annual_possible_savings:.2f}")
+    print(f"    Possible this year's savings: {annual_possible_savings:.2f}")
     total_possible_savings += annual_possible_savings
+    print(f"    Possible total electrical savings: {total_possible_savings:.2f}")
     annual_possible_savings *= 1 + annual_rate_increase
     opportunity_cost += opportunity_cost * investment_annual_return
     opportunity_return = opportunity_cost - battery_install_cost
     opportunity_cost = battery_install_cost + opportunity_return
-    print(f"    Possible opportunity return {opportunity_return:.2f}")
+    print(f"    Possible total opportunity return {opportunity_return:.2f}")
 
-    print(f"    No battery electrical cost for the year ${annual_no_battery:.2f} ")
+    print(f"    No battery additional electrical cost for the year ${annual_no_battery:.2f} ")
     total_no_battery_cost += annual_no_battery
     annual_no_battery *= 1 + annual_rate_increase
 
     new_battery_value -= new_battery_value * new_battery_annual_depreciation
-    print(f"    Battery value: {new_battery_value}")
+    print(f"    Current Battery value: {new_battery_value}")
+
+    electrical_savings_investment += total_possible_savings * investment_annual_return
+    print(f"    electrical savings earnings if invested {electrical_savings_investment:.2f}")
+
     print("")
 
-    if total_possible_savings >= battery_install_cost:
+    if (total_possible_savings + electrical_savings_investment) >= battery_install_cost:
         break
 
 print(f"Total possible savings {total_possible_savings:.2f}")
+total_possible_savings += electrical_savings_investment
+print(f"Total possible savings if invested: ${total_possible_savings:.2f}")
 
 opportunity_cost -= battery_install_cost
 print(f"Opportunity cost if you didn't buy a battery but invested the money. ${opportunity_cost:.2f}")
 
-print(f"But total cost of electricty without a battery ${total_no_battery_cost:.2f}")
+print(f"But total cost of electricity without a battery ${total_no_battery_cost:.2f}")
 real_opportunity_cost = (opportunity_cost - total_no_battery_cost) - new_battery_value
 if real_opportunity_cost > 0:
     print(f"You would be better off investing your money as you save ${real_opportunity_cost:.2f}")
